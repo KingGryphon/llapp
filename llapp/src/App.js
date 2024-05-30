3f5b53ee513f131e52d7608b6b891865793626a6
Archer Bergmann <archerbergmann@The-Kraken.ht.home>
2024-05-30 13:46:02 -0400

    the website runs at least

--- llapp/src/App.js
+++ llapp/src/App.js
@@ -1,7 +1,7 @@
-import Nav from '.components/Nav.js';
-import Main from '.components/Main.js';
-import Header from '.components/Header.js';
-import Footer from '.components/Footer.js';
+import Nav from './components/Nav';
+import Main from './components/Main';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 import './App.css';
 
